505972f9079f8d954b31e9cae48ed997d05a6933
prabhav bhattu <prabhavbhattu@gmail.com>
2021-11-28 09:10:37 -0700

    Added Slideshow to index.html

---
+++ slideshow.js
@@ -0,0 +1,34 @@
+var slide = 1;
+ss(slide);
+
+function fw(n) {
+    ss(slide += n);
+}
+
+function cs(n) {
+    ss(slide = n);
+}
+
+function ss(n) {
+    var i;
+    var slides = document.getElementsByClassName("i_ss");
+    var dots = document.getElementsByClassName("i_ss_dt");
+
+    if (n > slides.length) {
+        slide = 1
+    }
+
+    if (n < 1) {
+        slide = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slide - 1].style.display = "block";
+    dots[slide - 1].className += " active";
+  }
