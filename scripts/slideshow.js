306457891b3a2ea9131718d91110e074d7469841
prabhav bhattu <prabhavbhattu@gmail.com>
2021-11-29 11:21:46 -0700

    Optimized JS, fixed issue with only 3 slides showing, reorganized elements

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,13 +1,18 @@
 var slideIndex = 0;
+
 function updateSlides() {
 	let shows = Array.from(document.getElementsByClassName('slideshow'));
+
 	shows.forEach(show => {
 		let slides = Array.from(show.getElementsByClassName('slide'));
-		let realIndex = (slideIndex % (slides.length - 1))
-		slides.forEach((slide, i) => {slide.style.maxHeight = "0px"});
-		slides.forEach((slide, i) => {slide.style.opacity = "0.0"});
+		let realIndex = (slideIndex % slides.length)
+
+		slides.forEach(slide => slide.style.maxHeight = "0px");
+		slides.forEach(slide => slide.style.opacity = "0.0");
+
 		slides[realIndex].style.maxHeight = "100%";
 		slides[realIndex].style.opacity = "1.0";
+
 		slideIndex++;
 	})
 }
